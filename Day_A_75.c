fa04b8d0802da94e33f59e94f2ac37b09b56781b
Avinav-animeboy <akhilesh.thapa.at@gmail.com>
2026-04-24 08:11:47 +0530

    Create Day_A_75.c

---
+++ Day_A_75.c
@@ -0,0 +1,52 @@
+/*
+Problem Statement
+Given an array of integers containing both positive and negative values, find the length of the longest contiguous subarray whose sum is equal to zero.
+
+Input Format
+An integer array arr[].
+
+Output Format
+Print the length of the longest subarray with sum equal to zero.
+
+Sample Input
+15 -2 2 -8 1 7 10 23
+
+Sample Output
+5
+
+Explanation
+The subarray [-2, 2, -8, 1, 7] has a sum of 0 and is the longest such subarray.
+*/
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 1000
+
+int main() {
+    int arr[MAX_SIZE];
+    int n = 0;
+
+    // Read input
+    while (scanf("%d", &arr[n]) == 1) {
+        n++;
+    }
+
+    // Find the longest subarray with sum 0
+    int max_length = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            if (sum == 0) {
+                int length = j - i + 1;
+                if (length > max_length) {
+                    max_length = length;
+                }
+            }
+        }
+    }
+
+    printf("%d\n", max_length);
+    return 0;
+}
